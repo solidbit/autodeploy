dd54afbc61b0e7b1848e3075f5f398aef288e93c
Chang Wang <garnwraly@gmail.com>
2017-09-22 17:03:04 -0400

    docker setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,5 +6,13 @@ echo "cloning git repos"
 git clone git@github.com:NCI-GDC/signpost.git /var/deploy/signpost
 git clone git@github.com:NCI-GDC/gdcapi.git /var/deploy/gdcapi
 
+if [ -f /deploy-cache/virtual-envs/signpost/bin/activate ]; then
+  mkdir -p /deploy-cache/virtual-envs/signpost
+  virtualenv /deploy-cache/signpost
+
+fi
 echo "installing signpost"
+source /deploy-cache/virtual-envs/signpost/bin/activate
 cd /var/deploy/signpost
+pip install -r requirements.txt
+
