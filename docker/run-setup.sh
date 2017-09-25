de8a107a048f644d3f3f6ec92b8293bc0d0b6bbf
Chang Wang <garnwraly@gmail.com>
2017-09-25 09:53:58 -0400

    comment out running setup.sh (run this in the container to see logs)

--- docker/run-setup.sh
+++ docker/run-setup.sh
@@ -4,4 +4,4 @@ docker exec -d python mkdir -p /root/.ssh
 docker exec -d python mkdir -p /deploy-cache
 docker cp sshkey python:/root/.ssh/id_rsa
 docker cp setup.sh python:/setup.sh
-docker exec -d python sh -c 'sh /setup.sh'
+# docker exec -d python sh -c 'sh /setup.sh'
