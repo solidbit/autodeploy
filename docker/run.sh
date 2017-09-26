078316394adaa2f6bf27ff98624d45bc4f96c8ae
Chang Wang <garnwraly@gmail.com>
2017-09-26 16:52:07 -0400

    rename cache folder for run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,10 @@
+mkdir -p deploy-caches/gdcapi
+
+docker pull python:2.7.14
+
 docker run \
-  --mount type=bind,source="$(pwd)"/deploy-caches/prj1,target=/deploy-cache \
+  --mount type=bind,source="$(pwd)"/deploy-caches/gdcapi,target=/deploy-cache \
   --name python \
   --expose 5000 \
   -P \
   --rm -i -t python:2.7.14 bash
-
-
-  # -P \
-
-# python -m SimpleHTTPServer 5000
