56e3a5576d807c12add404eba48e8f6ebb7b27a2
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-11-27 17:28:08 -0500

    update previews

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const listContainers = require('docker/listContainers');
 
-const projectName = 'gdcapi';
 const hostname = process.env.HOST_NAME || 'localhost:3000';
 router.use('/hooks', require('./hooks'));
 
@@ -14,11 +13,17 @@ router.get('/containers', async (req, res) => {
 
 router.get('/deploys', async (req, res) => {
   const containers = await listContainers();
-  const containerInfo = containers.map(container => ({
-    status: container.id.Status,
-    state: container.id.State,
-    url: `http://${container.id.Names[0].slice(1)}--${projectName}.${hostname}`,
-  }));
+  const containerInfo = containers
+    .filter(container => container.id.Names[0].match(/_api_\d+$/))
+    .map(container => {
+      return {
+        status: container.id.Status,
+        state: container.id.State,
+        url: process.env.USE_SUBDOMAIN
+          ? `http://${container.id.Names[0].slice(1)}.${hostname}`
+          : `http://${hostname}/preview/${container.id.Names[0].slice(1)}`,
+      };
+    });
   res.send(containerInfo);
 });
 
