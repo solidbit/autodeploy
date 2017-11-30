41ec3dd8e9413a8953964a1963311f62c6318367
Chang Wang <garnwraly@gmail.com>
2017-11-30 12:20:06 -0500

    pm2 configs

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+const os = require('os');
+
+module.exports = {
+  apps: [
+    {
+      name: 'persona',
+      script: 'npm',
+      args: 'start',
+      watch: true,
+      ignore_watch: ['[\\/\\\\]\\./', 'node_modules', 'logs'],
+      exec_mode: 'fork',
+      restart_delay: 1000,
+      cron_restart: '5 3 * * 6',
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      pid_file: './logs/child.pid',
+      merge_logs: true,
+    },
+  ],
+};
